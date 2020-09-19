c5d533596213bc7a6efa4c4faf6b121311533ed7
bahaeddine <sihassi.bahaeddine@gmail.com>
2020-09-19 02:02:05 +0100

    Polymorphic relationships

---
+++ app/Post.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+
+    protected $guarded = [];
+
+
+    public function image()
+    {
+        return $this->morphOne(Image::class, 'imageable');
+    }
+
+    public function comments()
+    {
+        return $this->morphMany(Comment::class, 'commentable');
+    }
+
+    public function tags()
+    {
+        return $this->morphToMany(Tag::class, 'taggable');
+    }
+}
